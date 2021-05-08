0ed909b600145130732ea266a11faff5327a624a
nazo <44923159+nazo-x1@users.noreply.github.com>
2021-05-08 21:56:13 +0800

    disable nft-qos

--- devices/newifi-d2/diy.sh
+++ devices/newifi-d2/diy.sh
@@ -4,3 +4,5 @@ rm -Rf target/linux/ramips/.svn
 echo -e "\q" | svn co https://github.com/coolsnowwolf/lede/trunk/target/linux/ramips/patches-5.4 target/linux/ramips/patches-5.4
 
 sed -i 's?admin/status/channel_analysis??' package/feeds/luci/luci-mod-status/root/usr/share/luci/menu.d/luci-mod-status.json
+
+sed -i "s/CONFIG_PACKAGE_luci-app-nft-qos.*/CONFIG_PACKAGE_luci-app-nft-qos=n/g" .config
