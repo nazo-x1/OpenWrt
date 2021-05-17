7eeb7f0456f832c1db1f6689a447ef8a3721cfff
nazo <44923159+nazo-x1@users.noreply.github.com>
2021-05-18 01:20:03 +0800

    test lean's op

---
+++ devices/newifi-d2_lean/diy.sh
@@ -0,0 +1,11 @@
+find target/linux/ramips/* -maxdepth 0 ! -path '*/patches-5.4' -exec rm -Rf '{}' \;
+echo -e "\q" | svn co https://github.com/coolsnowwolf/lede/trunk/target/linux/ramips target/linux/ramips
+rm -Rf target/linux/ramips/.svn
+echo -e "\q" | svn co https://github.com/coolsnowwolf/lede/trunk/target/linux/ramips/patches-5.4 target/linux/ramips/patches-5.4
+
+sed -i 's?admin/status/channel_analysis??' package/feeds/luci/luci-mod-status/root/usr/share/luci/menu.d/luci-mod-status.json
+
+# 此阶段对 config 的修改是无效的
+# sed -i "s/CONFIG_PACKAGE_luci-app-nft-qos.*/CONFIG_PACKAGE_luci-app-nft-qos=n/g" .config
+
+rm -Rf package/lean
