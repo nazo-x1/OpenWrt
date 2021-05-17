f15602740bf41f44df52fd0f8859aaee01920697
nazo <44923159+nazo-x1@users.noreply.github.com>
2021-05-18 02:57:07 +0800

    Update diy.sh

--- devices/newifi-d2/diy.sh
+++ devices/newifi-d2/diy.sh
@@ -3,8 +3,6 @@ echo -e "\q" | svn co https://github.com/coolsnowwolf/lede/trunk/target/linux/ra
 rm -Rf target/linux/ramips/.svn
 echo -e "\q" | svn co https://github.com/coolsnowwolf/lede/trunk/target/linux/ramips/patches-5.4 target/linux/ramips/patches-5.4
 
-svn co https://github.com/immortalwrt/immortalwrt/branches/master/package/lean/mt-drivers package/lean/mt-drivers
-
 # sed -i 's?admin/status/channel_analysis??' package/feeds/luci/luci-mod-status/root/usr/share/luci/menu.d/luci-mod-status.json
 
 # 此阶段对 config 的修改是无效的
