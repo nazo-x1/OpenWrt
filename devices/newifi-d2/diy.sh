ed97f291053e2a63d636badfdb98d5a712274bb1
nazo <44923159+nazo-x1@users.noreply.github.com>
2021-05-19 21:46:30 +0800

    Update diy.sh

--- devices/newifi-d2/diy.sh
+++ devices/newifi-d2/diy.sh
@@ -3,8 +3,6 @@ echo -e "\q" | svn co https://github.com/coolsnowwolf/lede/trunk/target/linux/ra
 rm -Rf target/linux/ramips/.svn
 echo -e "\q" | svn co https://github.com/coolsnowwolf/lede/trunk/target/linux/ramips/patches-5.4 target/linux/ramips/patches-5.4
 
-# sed -i 's?admin/status/channel_analysis??' package/feeds/luci/luci-mod-status/root/usr/share/luci/menu.d/luci-mod-status.json
-
 # 此阶段对 config 的修改是无效的
 # sed -i "s/CONFIG_PACKAGE_luci-app-nft-qos.*/CONFIG_PACKAGE_luci-app-nft-qos=n/g" .config
 
@@ -13,12 +11,3 @@ sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_genera
 
 # Modify hostname
 sed -i 's/OpenWrt/Newifi-D2/g' package/base-files/files/bin/config_generate
-
-rm -rf include/kernel-version.mk
-wget -O include/kernel-version.mk https://raw.githubusercontent.com/coolsnowwolf/lede/master/include/kernel-version.mk
-
-sed -i 's?admin/status/channel_analysis??' package/feeds/luci/luci-mod-status/root/usr/share/luci/menu.d/luci-mod-status.json
-rm -rf include/kernel-version.mk
-wget -O include/kernel-version.mk https://raw.githubusercontent.com/coolsnowwolf/lede/master/include/kernel-version.mk
-
-sed -i 's?admin/status/channel_analysis??' package/feeds/luci/luci-mod-status/root/usr/share/luci/menu.d/luci-mod-status.json
