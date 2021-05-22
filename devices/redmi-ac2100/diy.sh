981c593ecf255ab0b2cd50cbaf45ad98ea6c2f33
nazo <44923159+nazo-x1@users.noreply.github.com>
2021-05-22 14:14:45 +0800

    Update diy.sh

--- devices/redmi-ac2100/diy.sh
+++ devices/redmi-ac2100/diy.sh
@@ -13,6 +13,9 @@ echo "src-git small https://github.com/kenzok8/small" >> feeds.conf
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 
+sed -i 's,$(STAGING_DIR_HOST)/bin/upx,upx,' package/feeds/small/*/Makefile
+sed -i 's,$(STAGING_DIR_HOST)/bin/upx,upx,' package/community/*/Makefile
+
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
 
