0631c0eee9edddc2da02785c4c9ffa3abfb2ea0a
nazo <44923159+nazo-x1@users.noreply.github.com>
2021-05-22 13:02:17 +0800

    change place

--- devices/newifi-d2/diy.sh
+++ devices/newifi-d2/diy.sh
@@ -6,10 +6,13 @@ echo -e "\q" | svn co https://github.com/coolsnowwolf/lede/trunk/target/linux/ra
 # 此阶段对 config 的修改是无效的
 # sed -i "s/CONFIG_PACKAGE_luci-app-nft-qos.*/CONFIG_PACKAGE_luci-app-nft-qos=n/g" .config
 
-echo "src-git small https://github.com/kenzok8/small" >> feeds.conf
+echo "src-git small https://github.com/kenzok8/small" >>feeds.conf
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
+
+sed -i 's,$(STAGING_DIR_HOST)/bin/upx,upx,' package/feeds/small/*/Makefile
+sed -i 's,$(STAGING_DIR_HOST)/bin/upx,upx,' package/community/*/Makefile
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
